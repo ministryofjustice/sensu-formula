4a2afaac04a7159b8ec99976b313fb26487e8899
Milos Gajdos <milosgajdos83@gmail.com>
2014-11-05 16:49:51 +0000

    Added sensu server tests.

---
+++ test/integration/default/serverspec/sensu_server_spec.rb
@@ -0,0 +1,35 @@
+require 'serverspec'
+require 'net/http'
+require 'uri'
+
+set :backend, :exec
+
+set :path, '/sbin:/usr/local/sbin:$PATH'
+
+describe "sensu server setup" do
+
+  sensu_conf = %w(redis.json rabbitmq.json api.json handlers.json).map{|c| "/etc/sensu/conf.d/" + c}
+  sensu_conf.each do |f|
+    describe file(f) do
+      it {should be_file}
+    end
+  end
+
+  describe file("/etc/apparmor.d/opt.sensu.embedded.bin.sensu-server") do
+    it {should be_file}
+  end
+
+  describe service("sensu-server") do
+    it {should be_enabled}
+    it {should be_running}
+  end
+
+  describe file("/etc/apparmor.d/opt.sensu.embedded.bin.sensu-api") do
+    it {should be_file}
+  end
+
+  describe service("sensu-api") do
+    it {should be_enabled}
+    it {should be_running}
+  end
+end
